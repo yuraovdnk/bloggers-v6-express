a44bd4cb2f79e89b33bb148690ad7621836442ea
Yurii <yuraovdnk1999@ukr.net>
2022-07-29 14:36:06 +0300

    fix db url

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 export const settings = {
     JWT_SECRET: process.env.JWT_SECRET || "123",
-    MONGODB:'mongodb+srv://yuraovdnk:uNYIBlYJI2PqWNrJ@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority',
+    MONGODB:'mongodb+srv://yuraovdnk:uNYIBlYJI2PqWNrJ@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority&wtimeoutMS=5000',
     //MONGODB:'mongodb://localhost:27017'
 }
